eeaa1490ea90e4c5e0ceea4067d6e6b257c2e926
simishu07 <simishu07@users.noreply.github.com>
2022-11-14 23:44:30 +0530

    Trying to update again.

--- react-portfolio-website-master/src/App.js
+++ react-portfolio-website-master/src/App.js
@@ -4,9 +4,10 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Skills from './components/Skills'
+import Portfolio from './components/Portfolio'
 //import Soundbar from './components/Soundbar'
 import './App.scss'
-import Portfolio from './components/Portfolio'
+//import Portfolio from './components/Portfolio'
 
 function App() {
   return (
