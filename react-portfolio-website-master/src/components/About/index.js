910d879ae370cdafbccc59b84856eb4d97aeab17
simishu07 <simishu07@users.noreply.github.com>
2022-11-15 15:05:39 +0530

    Added missed class

--- react-portfolio-website-master/src/components/About/index.js
+++ react-portfolio-website-master/src/components/About/index.js
@@ -9,6 +9,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const About = () => {
